66880b402c9fb126346483fea667d2f697cafaea
Abhinav Yadav <abhiydv178@gmail.com>
2026-06-17 18:01:08 +0530

    fixed login errors

--- Day 1/Tip Calculator/script.js
+++ Day 1/Tip Calculator/script.js
@@ -1,21 +1,14 @@
-const button = document.getElementById("calculate");
+const button = document.getElementById("Calculate");
 const displayBill = document.getElementById("total-bill-amount");
 const displayTip = document.getElementById("tip-amount");
 const displayTippp = document.getElementById("tip-pp-amount");
 const displayTotalpp = document.getElementById("total-pp-amount");
 function calc() {
-  const bill = parseInt(document.getElementById("billamount").value);
-  const tip = parseInt(document.getElementById("tippercentage").value);
-  const persons = parseInt(document.getElementById("persons").value);
+  const bill = parseFloat(document.getElementById("billamount").value) || 0;
+  const tip = parseFloat(document.getElementById("tippercentage").value) || 0;
+  const persons = parseFloat(document.getElementById("persons").value) || 1;
   console.log(bill);
-  if (
-    isNaN(bill) ||
-    isNaN(tip) ||
-    isNaN(persons) ||
-    persons <= 0 ||
-    tip < 0 ||
-    persons < 0
-  ) {
+  if (persons <= 0 || tip < 0 || bill < 0) {
     document.getElementById("billamount").value = "";
     document.getElementById("tippercentage").value = "15";
     document.getElementById("persons").value = "1";
@@ -32,5 +25,6 @@ function calc() {
   displayTotalpp.textContent = `${totalpp.toFixed(2)}`;
 }
 
-button.addEventListener("click", calc);
-// document.getElementById("billamount").addEventListener("input", calc);
+document.getElementById("billamount").addEventListener("input", calc);
+document.getElementById("tippercentage").addEventListener("input", calc);
+document.getElementById("persons").addEventListener("input", calc);
