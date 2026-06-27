df76aebfd75f2e43235e74919724fffcffe46214
Abhinav Yadav <abhiydv178@gmail.com>
2026-06-27 19:29:52 +0530

    added components

--- Day4/MovieSearchApp/src/App.jsx
+++ Day4/MovieSearchApp/src/App.jsx
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen bg-slate-900 text-white font-sans">
+      <Navbar>
+        <SearchBar onSearch={handleSearch} />
+      </Navbar>
+
+      <main className="container mx-auto px-4 py-8">
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <MovieGrid movies={movies} onMovieClick={setSelectedMovie} />
+        )}
+      </main>
+
+      {/* Conditionally rendered overlays/drawers */}
+      {isFavoritesOpen && <FavoritesDrawer />}
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
